9af9e0dcfd87418a3344565229629c6d449ad091
HrutikS <hrutiksurwade.work@gmail.com>
2024-06-19 01:00:54 +0530

    Added - Product model and create Product endpoint

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
+//Express
 const express = require('express');
 const app = express();
 
-app.get('/get', (req, res)=> {
-    res.send('Hello from node!');
+//Middleware so that application understands json
+app.use(express.json());
+
+//Mongoose
+const mongoose = require('mongoose');
+
+//Product 
+const product = require('./models/product');
+const Product = require('./models/product');
+
+//Routes
+// app.get('/get', (req, res)=> {
+//     res.send('Hello from node!');
+// })
+
+//Create product endpoint
+app.post('/product', async(req, res)=> {
+    
+    try {
+
+        const product = await Product.create(req.body)
+        res.status(201).json(product);
+        
+    } catch (error) {
+        
+        console.log("ERROR : "+error.message);
+        res.status(500).json({message: error.message});
+    }    
 })
 
-app.listen(3000, ()=> {
-    console.log('Node server started at port 3000');
-})
+//MongoDb connection
+mongoose.connect('mongodb://0.0.0.0:27017/node-apis') //connection url from mongodb compass contained 'localhost'. However, using 'localhost' here throws error hence used '0.0.0.0'
+    .then(()=> {
+        
+        console.log("Successfully connected to MongoDb");
+        
+        app.listen(3000, ()=> {
+            console.log('Node server started at port 3000');
+        })
+
+    }).catch((error)=> {
+        console.log(error);
+    })
