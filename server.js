d50d9fde639668dcb58eaf13a28e54a24d3a8971
HrutikS <hrutiksurwade.work@gmail.com>
2024-06-20 01:22:47 +0530

    Added - Update & Delete product endpoints

--- server.js
+++ server.js
@@ -61,6 +61,53 @@ app.get('/products/:id', async(req,res)=> {
     }
 })
 
+//Update product
+app.put('/products/:id', async(req,res) => {
+
+    try {
+
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        //if product is empty or not found
+        if(!product){
+            res.status(404).json({message: `Product not found. Invalid Id.`});
+            console.log('Product not found. Invalid Id.');
+        }
+
+        const updatedProduct = await Product.findById(id);  
+        res.status(200).json(updatedProduct);
+        
+    } catch (error) {
+
+        console.log("ERROR : "+error.message);
+        res.status(500).json({message:error.message});        
+    }
+})
+
+//Delete product
+app.delete('/products/:id', async(req,res)=> {
+    
+    try {
+
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        //if product is empty or not found
+        if(!product){
+            res.status(404).json({message: `Product not found. Invalid Id.`});
+            console.log('Product not found. Invalid Id.');
+        }
+
+        res.status(200).json({message: `Product with Id :`+id+` deleted successfully.`});
+        
+    } catch (error) {
+        
+        console.log('ERROR :'+error.message);
+        res.status(500).json({message: error.message});
+    }
+})
+
 //MongoDb connection
 mongoose.connect('mongodb://0.0.0.0:27017/node-apis') //connection url from mongodb compass contained 'localhost'. However, using 'localhost' here throws error hence used '0.0.0.0'
     .then(()=> {
